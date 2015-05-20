75f46b155a557c68cb234187962eca1d7d5eab9c
erpreciso <trepreciso@gmail.com>
2015-05-20 19:25:53 +0200

    Create app idea

---
+++ chuck-norris-app-sandbox.R
@@ -0,0 +1,12 @@
+
+# install.packages("tm.lexicon.GeneralInquirer", repos="http://datacube.wu.ac.at", type="source")
+require(tm)
+require("tm.lexicon.GeneralInquirer")
+require(RCurl)
+
+positive.terms <- terms_in_General_Inquirer_categories("Positiv")
+negative.terms <- terms_in_General_Inquirer_categories("Negativ")
+
+t <- getURL("http://api.icndb.com/jokes/random/")
+q <- fromJSON(t)
+tm_term_score(PlainTextDocument(q$value$joke), negative.terms)
